62516f30d6af3e646678e684dae0ff0ca747bfac
4phation <143794918+4phation@users.noreply.github.com>
2023-11-09 20:45:03 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,5 +1,17 @@
+apt install masscan nikto sqlmap whatweb nmap
+
 wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
+
+git clone https://github.com/guelfoweb/knock.git
+git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/aboul3la/Sublist3r.git sublist3r
+
+
+
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
